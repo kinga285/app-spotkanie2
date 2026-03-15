060dbe68b29133de0073057b23c79908b6b11a50
kinga285 <45886750+kinga285@users.noreply.github.com>
2026-03-15 12:12:34 +0100

    Nowy program ze spotkania nr 2

---
+++ Spotkanie2/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int wiek1 = 0; //deklaracja z inicjalizacją, póki zmienna nie ma żadnej wartości nie jest zainicjalizowana
+
+//nadpisanie zmiennej - literał to kawałek danych - liczba, litera, słowo
+//literały są łączone w wyrażenie poprzez operatory np. +
+//dobrą praktyką jest uruchamianie kodu co kilkanaście linijek (aby znaleźć błąd)
+wiek1 = 10+20; 
+
+
+// 2. Typy danych
+// pascal case = alaMaKota; konwencja pisania w C#; Java 
+// Typy proste (primitive) - tylko dane np. liczba, pojednynczy znak 
+
+int age = 25; //ważne
+long duzaliczba = 4475493;
+
+double temp = 25.5; //ważne
+
+char znak = 'A'; //pojedynczy znak
+
+bool czyPadaDescz = true; 
+
+// Typy złożone (complex) - dane + zachowanie (metody)
+
+string zdanie = "Ala ma kota";
+
+string noweZdanie = zdanie.ToUpper(); // wywołanie metody dedykowanej dla typu danych string. Metoda ToUpper dostaje na wejście zmienną zdanie
+
+
+// 3.  Pułapki
+double d1 = 0.1;
+double d2 = 0.2;
+
+double wynik = d1 + d2; //0,30000000000000004
+
+// Można przekonwertowac na liczbe całkowitą i później przeskalować
+// Albo wykorzystac specjalną bibliotekę 
+
+
+// 3.2 
+int l1 = 10;
+double l2 = 10.5;
+//int wynik2 = l1 + l2; //błąd, bo zmienna wynikowa int + double = double
+int wynik2 = (int)(l1 + l2); // rzutowanie (casting) - zamiana jednego typu danych na drugi, analogia do CAST w sql
+//int wynik2 = l1 + (int)l2; // rzutowanie (casting) - zamiana jednego typu danych na drugi
+
+// Parsowanie (parse) - zamiana string -> inny typ
+// Proces złożony, może powodowac błędy
+// "43.4" -> 43.4
+
+string liczba = "450";
+int liczba2 = int.Parse(liczba);
+
+//Console.WriteLine(liczba2+10);
+
+// Konkatenacja
+string imie1 = "Ala";
+int liczbaKotow = 15;
+string zwierze = "kota";
+string zdanie2 = imie1+" ma "+liczbaKotow+" "+zwierze; 
+
+//Console.WriteLine(zdanie2);
+
+
+double malaLiczba = 10.34467532;
+double zaokraglona = Math.Round(malaLiczba, 3);
+//Console.WriteLine(zaokraglona);
+
+
+//Program 1
+// Zapytaj o imie i wiek , nastepnie wypisz je na konsole
+
+Console.WriteLine("Podaj swoje imie:");
+string imieUzytkownika = Console.ReadLine();
+
+Console.WriteLine("Podaj swój wiek:");
+int wiekUzytkownika = int.Parse(Console.ReadLine());
+
+int rokUrodzenia = DateTime.UtcNow.Year - wiekUzytkownika;
+
+Console.WriteLine("Imie użytkownika to " + imieUzytkownika + "\nUrodziłaś się w roku " + rokUrodzenia );
+
+
+//Program 2 - instrukcje warunkowe
+//Wpisz dwie liczby - ile punktów z testu otrzymano; ile było maksymalnie do zdobycia
+//Wypisz uzyskaną ocenę
+//>=50% - 3.0 itd
+
